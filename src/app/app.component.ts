c18c3d268d91a2083048f2384a61c7cb603ed746
Nitin Nair <nitin9nair@outlook.com>
2018-10-16 21:46:57 +0530

    added routing module and modified signup and signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bookmarkr';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBkjVphQERLb991Sd_CXtf3cKY2BH5fsAA",
+      authDomain: "bookmarkr-3c56f.firebaseapp.com",
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -10,6 +11,9 @@ import { FooterComponent } from './core/footer/footer.component';
 import { BookmarkListComponent } from './bookmarks/bookmark-list/bookmark-list.component';
 import { BookmarkAddComponent } from './bookmarks/bookmark-add/bookmark-add.component';
 import { BookmarkEditComponent } from './bookmarks/bookmark-edit/bookmark-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth/auth.service';
+
 
 @NgModule({
   declarations: [
@@ -24,9 +28,11 @@ import { BookmarkEditComponent } from './bookmarks/bookmark-edit/bookmark-edit.c
     BookmarkEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
