5a50f98059f3a223db351b17ab504743fc06fc7c
Nitin Nair <nitin9nair@outlook.com>
2019-02-26 23:34:16 +0530

    added hash location for URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
     exports: [RouterModule]
 })
 
--- src/app/auth/settings/settings.component.ts
+++ src/app/auth/settings/settings.component.ts
@@ -12,7 +12,7 @@ export class SettingsComponent implements OnInit {
   oldnewSame:boolean = false;
   newcnfSame:boolean = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
